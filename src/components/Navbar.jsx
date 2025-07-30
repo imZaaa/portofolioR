76cc8fb96a93d890f79ee794f14635463abea646
imZaaa <rhezarifalsya266@gmail.com>
2025-07-31 00:18:36 +0700

    'p'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
+import { Link } from "react-scroll";
 import "../assets/styles/style.css";
 
 const Navbar = () => {
-
   return (
     <nav className="navbar navbar-expand-lg floating-navbar shadow-sm">
       <div className="container d-flex align-items-center justify-content-between position-relative">
@@ -10,12 +10,26 @@ const Navbar = () => {
         <a className="navbar-brand fw-bold" href="#">Zaa</a>
 
         {/* Navbar Links */}
-        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarNav"
+        >
           <ul className="navbar-nav d-flex gap-lg-4 gap-2 text-center">
-            <li className="nav-item"><a className="nav-link" href="#home">Home</a></li>
-            <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
-            <li className="nav-item"><a className="nav-link" href="#projects">Projects</a></li>
-            <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+            {["home", "about", "projects", "contact"].map((section) => (
+              <li className="nav-item" key={section}>
+                <Link
+                  className="nav-link"
+                  activeClass="active"
+                  to={section}
+                  spy={true}
+                  smooth={true}
+                  offset={-70} // biar gak ketutup navbar
+                  duration={600}
+                >
+                  {section.charAt(0).toUpperCase() + section.slice(1)}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
 
